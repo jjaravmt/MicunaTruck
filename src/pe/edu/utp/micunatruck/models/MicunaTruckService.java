b0cdb90ee7fdbcf3c88dbfe90ac7bbac15763fb9
rollys <rolly.sanchez.26@gmail.com>
2018-02-21 21:44:43 -0500

    Agregando funcionalidades para eventos en el service

--- src/pe/edu/utp/micunatruck/models/MicunaTruckService.java
+++ src/pe/edu/utp/micunatruck/models/MicunaTruckService.java
@@ -92,4 +92,21 @@ public class MicunaTruckService {
     public UserType findUserTypeById(int id){
         return getUsersTypeEntity() != null ? getUsersTypeEntity().findById(id) : null;
     }
+
+
+
+    public Event createEvent(User user, EventStatus eventStatus, String name, String description, String image) {
+        return getEventsEntity() != null ?
+                getEventsEntity().create(user, eventStatus, name, description, image) : null;
+    }
+
+    public boolean deleteEvent(int id) {
+        return getEventsEntity() != null ?
+                getEventsEntity().delete(id) : false;
+    }
+
+    public boolean updateEvent(Event event, User user, EventStatus eventStatus) {
+        return getEventsEntity() != null ?
+                getEventsEntity().update(event, user, eventStatus) : false;
+    }
 }
