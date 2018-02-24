9f1f085f6ee1dc6f7e4bb98f717cc70809aeb40d
Miguel Ángel Poma Hinostroza <pomahinostroza.miguel@gmail.com>
2018-02-24 02:42:57 -0500

    Se creó el UsersBean.

---
+++ src/pe/edu/utp/micunatruck/beans/UsersBean.java
@@ -0,0 +1,149 @@
+package pe.edu.utp.micunatruck.beans;
+
+import pe.edu.utp.micunatruck.models.MicunaTruckService;
+import pe.edu.utp.micunatruck.models.User;
+import pe.edu.utp.micunatruck.models.UserType;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class UsersBean implements Serializable {
+    private MicunaTruckService micunaTruckService;
+    private User user;
+    private UserType userType;
+
+    public UsersBean(){
+        micunaTruckService = new MicunaTruckService();
+    }
+
+    public List<User> getUsers() {
+        return micunaTruckService.findAllUsers();
+    }
+
+    public void setUserType(UserType userType){
+        this.userType = userType;
+    }
+
+    public UserType getUserType(){
+        return  this.userType;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getName(){
+        return this.getUser().getName();
+    }
+
+    public void setName(String name){
+        this.getUser().setName(name);
+    }
+
+    public String getLastName(){
+        return this.getUser().getLastName();
+    }
+
+    public void setLastName(String lastName){
+        this.getUser().setLastName(lastName);
+    }
+
+    public String getLegalName(){
+        return this.getUser().getLastName();
+    }
+
+    public void setLegalName(String legalName){
+        this.getUser().setLegalName(legalName);
+    }
+
+    public String getDescription(){
+        return this.getUser().getDescription();
+    }
+
+    public void setDescription(String description){
+        this.getUser().setDescription(description);
+    }
+
+    public String getPhoto(){
+        return this.getUser().getPhoto();
+    }
+
+    public void setPhoto(String photo){
+        this.getUser().setPhoto(photo);
+    }
+
+    public String getAddress(){
+        return this.getUser().getAddress();
+    }
+
+    public void setAddress(String address){
+        this.getUser().setAddress(address);
+    }
+
+    public String getTelephone(){
+        return this.getUser().getTelephone();
+    }
+
+    public void setTelephone(String telephone){
+        this.getUser().setTelephone(telephone);
+    }
+
+    public String getEmail(){
+        return this.getUser().getEmail();
+    }
+
+    public void setEmail(String email){
+        this.getUser().setEmail(email);
+    }
+
+    public String getPassword(){
+        return this.getUser().getPassword();
+    }
+
+    public void setPassword(String password){
+        this.getUser().setPassword(password);
+    }
+
+    public boolean getFlagActive(){
+        return this.getUser().getFlagActive();
+    }
+
+    public void setFlagActive(boolean flagActive){
+        this.getUser().setFlagActive(flagActive);
+    }
+
+    public String editUser(User user){
+        this.setUser(user);
+        return "success";
+    }
+
+    public String newEvent(){
+        this.setUser(new User());
+        return "success";
+    }
+    public String createUser() {
+        micunaTruckService.createUser(this.getUserType(), this.getName(), this.getLastName(),
+                this.getLegalName(),
+                this.getDescription(), this.getPhoto(), this.getAddress(), this.getTelephone(),
+                this.getEmail(), this.getPassword(), this.getFlagActive());
+        return "success";
+    }
+
+    public String updateUser() {
+        micunaTruckService.updateUser(this.getUser(), this.getUserType());
+        return "success";
+    }
+
+    public String deleteUser(User user) {
+        micunaTruckService.deleteUser(user.getId());
+        return "success";
+    }
+}
